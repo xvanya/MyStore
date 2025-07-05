60e5a52a8880377b3035183103a7bc9367b90002
xnetvoya <xnetvoyax@gmail.com>
2025-07-05 10:44:20 +0200

    Working with google auth

--- Django RESTAPI/mystore/mystore/urls.py
+++ Django RESTAPI/mystore/mystore/urls.py
@@ -16,16 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-from rest_framework.routers import DefaultRouter
-
 from product.views import CategoryViewSet
+from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
+from product.views import GoogleLoginView
 
 router = DefaultRouter()
-router.register('categories', CategoryViewSet, basename='category')
+router.register(r'categories', CategoryViewSet, basename='category')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-
-]
+    path('api/google-login/', GoogleLoginView.as_view(), name='google-login'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
--- Django RESTAPI/mystore/product/views.py
+++ Django RESTAPI/mystore/product/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from .models import Category
 from .serializers import CategorySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import requests
 
-# Create your views here.
-class CategoryViewSet(ModelViewSet):
+GOOGLE_USERINFO_URL = "https://www.googleapis.com/oauth2/v3/userinfo"
+
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+class GoogleLoginView(APIView):
+    def post(self, request):
+        code = request.data.get('code')
+        if not code:
+            return Response({'error': 'Code is required'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        resp = requests.get(GOOGLE_USERINFO_URL, headers={"Authorization": f"Bearer {code}"})
+        if resp.status_code != 200:
+            return Response({"detail": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
+
+        userinfo = resp.json()
+        return Response(userinfo)
